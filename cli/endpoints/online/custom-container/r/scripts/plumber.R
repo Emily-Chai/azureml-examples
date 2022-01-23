69144729a2aa43cda5fc79a918f8d658e9d0cc0c
Emily-Chai <57716190+Emily-Chai@users.noreply.github.com>
2022-01-23 18:09:56 -0500

    Add files via upload

--- cli/endpoints/online/custom-container/r/scripts/plumber.R
+++ cli/endpoints/online/custom-container/r/scripts/plumber.R
@@ -1,6 +1,7 @@
 # plumber.R
 
-accident_model <- readRDS("model.rds")
+## Load the model
+modellist = readRDS("irismodel.rds")
 
 #* Liveness check
 #* @get /live
@@ -14,26 +15,26 @@ function() {
   "ready"
 }
 
-#* Returns likelihood of fatality given information about a car accident
-#* @param dvcat, velocity, possible values "1-9km/h" "10-24"   "25-39"   "40-54"   "55+"  
-#* @param seatbelt, possible values "none" "belted"  
-#* @param frontal, possible values "notfrontal" "frontal"
-#* @param sex, possible values "f" "m"
-#* @param ageOfocc, possible values age in years, 16-97
-#* @param yearVeh, year of vehicle, possible values 1955-2003
-#* @param airbag, was an airbag present? possible values "none" "airbag"
-#* @param occRole, possible values "driver" "pass" 
-#* @post /score
-function(dvcat, seatbelt, frontal, sex, ageOfocc, yearVeh, airbag, occRole) {
-  newdata <- data.frame(
-    dvcat=dvcat,  
-    seatbelt=seatbelt,
-    frontal=frontal,
-    sex=sex,
-    ageOFocc=as.integer(ageOfocc),
-    yearVeh=as.integer(yearVeh),
-    airbag=airbag,
-    occRole=occRole
-    )
-  as.numeric(predict(accident_model, newdata, type="response") * 100)
+
+## Lets make the predictions
+#* @param Sepal.Length
+#* @param Sepal.Width
+#* @param Petal.Length
+#* @param Petal.Width
+#* @get /predict
+predictions <- function(Sepal.Length, Sepal.Width, Petal.Length, Petal.Width){
+  Sepal.Length <- as.numeric(Sepal.Length )
+  Sepal.Width <-  as.numeric(Sepal.Width)
+  Petal.Length  <- as.numeric(Petal.Length)
+  Petal.Width <-  as.numeric(Petal.Width)
+  
+  X.new <- data.frame(Sepal.Length= Sepal.Length,
+                      Sepal.Width = Sepal.Width ,
+                      Petal.Length=Petal.Length,
+                      Petal.Width = Petal.Width )
+  
+  #predict based on input
+  ##predict(iris_rf, new_data= X.new, type ="class")
+  y.pred <- modellist$NewPredictions(model = modellist$modelobject, newdata = X.new)
+  return(y.pred)
 }
